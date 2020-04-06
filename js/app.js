955c87afc06b203f941ebc3c0165b3dca555a9f1
Yancarlos <yeg003@gmail.com>
2020-04-05 20:28:33 -0400

    smooth scroll added on navbar

--- js/app.js
+++ js/app.js
@@ -21,17 +21,21 @@
 
 const sectionElement = document.querySelectorAll('section');
 const createNav = document.querySelector('#navbar__list');
+const menuLink = document.querySelectorAll('.menu__link')
+const dataNavs = document.querySelectorAll('[data-nav]')
+
 /**
  * End Global Variables*/
 
 // build the nav by iterating through the sections on the page
 
-sectionElement.forEach( function(e){
+sectionElement.forEach( (e) => {
     //create li element
     let createdLi = document.createElement('li');
 
     // store data-nav attribute in a value
     let dataNav = e.getAttribute('data-nav');
+    
 
     // add class to li
     createdLi.classList.add('menu__link');
@@ -52,21 +56,45 @@ sectionElement.forEach( function(e){
     createdLi.appendChild(anchorTag);
 });
 
+//  logic for sections position in viewport
+
+function isInViewport(e) { 
+    let rect = e.getBoundingClientRect();
+    return(
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && 
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  
+}
 
 // Add class 'active' to section when near top of viewport
 
-// Scroll to anchor ID using scrollTO event
+window.addEventListener('scroll', toggleClassFunction);
+    function toggleClassFunction()  { 
+        // iterates through all sections and adds/ removes class
+        for (i = 0; i < sectionElement.length; i++) {
+            if(!isInViewport(sectionElement[i])){
+                sectionElement[i].classList.remove('your-active-class');
+            } else {
+                sectionElement[i].classList.add('your-active-class');
+            }
+        }
+    }
 
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
+// Smooth scroll to section when anchor is clicked
 
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
+const aElement = document.getElementsByTagName('a')
+for (let i = 0; i < sectionElement.length; i++) {
+    let element = sectionElement[i];
+    let anchor = aElement[i];
+    // click event listener with smooth scroll
+    anchor.addEventListener('click', (e) => {
+        e.preventDefault();
+        element.scrollIntoView({behavior: "smooth" });
+    });
+    
+}
 
 
