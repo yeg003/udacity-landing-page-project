92b117fa599f8531d25cea73ee33a21393da5f24
Yancarlos <yeg003@gmail.com>
2020-04-06 13:18:11 -0400

    updated README file

--- js/app.js
+++ js/app.js
@@ -21,33 +21,22 @@
 
 const sectionElement = document.querySelectorAll('section');
 const createNav = document.querySelector('#navbar__list');
+const liTag= document.querySelectorAll('li')
+
 /**
  * End Global Variables*/
 
 // build the nav by iterating through the sections on the page
-
 sectionElement.forEach( (e) => {
-    //create li element
     let createdLi = document.createElement('li');
-
     // store data-nav attribute in a value
     let dataNav = e.getAttribute('data-nav');
-
-    // add class to li
     createdLi.classList.add('menu__link');
-
-    //append li to ul
     createNav.appendChild(createdLi);
-
-    //create anchor tag
     let anchorTag = document.createElement("a");
-
     // iterate href on anchor
     anchorTag.href =  "#" +  e.id;
-
-    //add content to anchor
     anchorTag.textContent = dataNav;
-    
     //add anchor to li
     createdLi.appendChild(anchorTag);
 });
@@ -77,6 +66,8 @@ window.addEventListener('scroll', toggleClassFunction);
             }
         }
     }
+    
+    
 
 // Smooth scroll to section when anchor is clicked
 
